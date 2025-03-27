b30c9d91862a886e6257a5fd11527b1247aae237
aan-cloud <evirsanti@gmail.com>
2025-03-27 22:42:05 +0800

    chore: add cart class request

--- src/main/java/com/cheffest/customer_service_api100/request/CartRequest.java
+++ src/main/java/com/cheffest/customer_service_api100/request/CartRequest.java
@@ -1,4 +1,23 @@
 package com.cheffest.customer_service_api100.request;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
 public class CartRequest {
+    private UUID userId;
+    private UUID foodId;
+    private int sum;
+
+    // Constructor
+    public CartRequest() {}
+
+    public CartRequest(UUID userId, UUID foodId, int sum) {
+        this.userId = userId;
+        this.foodId = foodId;
+        this.sum = sum;
+    }
 }
