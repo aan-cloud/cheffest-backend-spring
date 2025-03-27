f99cde6695b3071a7a591077425840747dacd1b1
aan-cloud <evirsanti@gmail.com>
2025-03-28 00:36:18 +0800

    fix: env production

--- src/main/java/com/cheffest/customer_service_api100/Application.java
+++ src/main/java/com/cheffest/customer_service_api100/Application.java
@@ -7,15 +7,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
-		// Muat file .env dan set system properties
-		Dotenv dotenv = Dotenv.load();
+		// Load .env file, but ignore if it's missing (useful for production)
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
 		System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
 		System.setProperty("DATABASE_USERNAME", dotenv.get("DATABASE_USERNAME"));
 		System.setProperty("DATABASE_PASSWORD", dotenv.get("DATABASE_PASSWORD"));
 		System.setProperty("GOOGLE_CLIENT_ID", dotenv.get("GOOGLE_CLIENT_ID"));
 		System.setProperty("GOOGLE_CLIENT_SECRET", dotenv.get("GOOGLE_CLIENT_SECRET"));
 
-		// Lanjutkan dengan inisialisasi Spring Boot
+		// Continue with Spring Boot initialization
 		SpringApplication.run(Application.class, args);
 	}
 }
