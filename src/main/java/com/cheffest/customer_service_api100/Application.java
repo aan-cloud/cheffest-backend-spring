d35b561fab3eeb28d5070d576a1660f397d435d3
aan-cloud <evirsanti@gmail.com>
2025-03-27 22:36:54 +0800

    feat: main class spring-boot app

---
+++ src/main/java/com/cheffest/customer_service_api100/Application.java
@@ -0,0 +1,13 @@
+package com.cheffest.customer_service_api100;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
