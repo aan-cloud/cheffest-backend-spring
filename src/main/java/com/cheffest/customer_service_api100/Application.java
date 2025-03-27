092a41539d4c224a765d09cc36eb1d338227859a
aan-cloud <evirsanti@gmail.com>
2025-03-28 00:11:28 +0800

    fix: env

--- src/main/java/com/cheffest/customer_service_api100/Application.java
+++ src/main/java/com/cheffest/customer_service_api100/Application.java
@@ -1,13 +1,21 @@
 package com.cheffest.customer_service_api100;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-
 	public static void main(String[] args) {
+		// Muat file .env dan set system properties
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
+		System.setProperty("DATABASE_USERNAME", dotenv.get("DATABASE_USERNAME"));
+		System.setProperty("DATABASE_PASSWORD", dotenv.get("DATABASE_PASSWORD"));
+		System.setProperty("GOOGLE_CLIENT_ID", dotenv.get("GOOGLE_CLIENT_ID"));
+		System.setProperty("GOOGLE_CLIENT_SECRET", dotenv.get("GOOGLE_CLIENT_SECRET"));
+
+		// Lanjutkan dengan inisialisasi Spring Boot
 		SpringApplication.run(Application.class, args);
 	}
-
 }
