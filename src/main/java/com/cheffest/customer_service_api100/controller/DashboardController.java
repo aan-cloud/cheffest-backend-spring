8f533149e82bad5e8371bc7adb93d3e59da0ac2b
aan-cloud <evirsanti@gmail.com>
2025-03-30 18:25:07 +0800

    chore: provide api test docs inn dashboard response

--- src/main/java/com/cheffest/customer_service_api100/controller/DashboardController.java
+++ src/main/java/com/cheffest/customer_service_api100/controller/DashboardController.java
@@ -8,6 +8,6 @@ public class DashboardController {
 
     @GetMapping("/")
     public String dashboard() {
-        return "Welcome to the cheffest API! You can see all the API documentation clearly on https://blabla.com";
+        return "Welcome to the cheffest API! You can see all the API documentation clearly on https://www.postman.com/joint-operations-pilot-8366866/cheffest-spring-api/request/ppmmu06/food?action=share&creator=36210259&ctx=documentation";
     }
 }
